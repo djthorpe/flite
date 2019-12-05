5fe249e17afd3ec867d84199dade3a2eb42e383f
David Thorpe <djt@mutablelogic.com>
2019-12-05 18:11:49 +1030

    Updated flite

--- Makefile
+++ Makefile
@@ -1,11 +1,18 @@
+# Where to find flite
+PREFIX="/opt"
+PKG_CONFIG_PATH="/opt/flite/pkgconfig"
+
 # Go parameters
 GOCMD=go
 GOINSTALL=$(GOCMD) install
 GOTEST=$(GOCMD) test
 GOCLEAN=$(GOCMD) clean
 
-# PKG_CONFIG_PATH
-PKG_CONFIG_PATH="/opt/flite/pkgconfig"
+all: distbuild test
+
+distbuild:
+	install -d $(PREFIX) -m 0775
+	scripts/install-flite.sh -p $(PREFIX)
 
 test:
 	PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(GOTEST) -v ./flite
--- flite/flite.go
+++ flite/flite.go
@@ -7,12 +7,19 @@
 
 package flite
 
+import (
+	"fmt"
+	"strconv"
+)
+
 ////////////////////////////////////////////////////////////////////////////////
 // CGO
 
 /*
 #cgo pkg-config: flite
 #include <flite/flite.h>
+
+cst_voice* register_cmu_us_awb();
 */
 import "C"
 
@@ -20,7 +27,9 @@ import "C"
 // TYPES
 
 type (
-	Error C.int
+	Error   C.int
+	Voice   C.cst_voice
+	WavFile C.cst_wave
 )
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -28,10 +37,11 @@ type (
 
 const (
 	FLITE_ERR_NONE Error = 0
+	FLITE_ERR_BADPARAM
 )
 
 ////////////////////////////////////////////////////////////////////////////////
-// FUNCTIONS
+// INIT
 
 func Init() error {
 	if err := Error(C.flite_init()); err != FLITE_ERR_NONE {
@@ -42,12 +52,47 @@ func Init() error {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// CRESTE
+
+func TextToSpeech(text string, voice *Voice) (*WavFile, error) {
+	if voice == nil {
+		return nil, FLITE_ERR_BADPARAM
+	} else {
+		cstr := C.CString(text)
+		defer C.free(cstr)
+		return C.flite_text_to_wave(cstr, voice)
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// VOICE
+
+func (this *Voice) Name() string {
+	voice := (*C.cst_voice)(this)
+	if this == nil || voice.name == nil {
+		return "<nil>"
+	} else {
+		return C.GoString(voice.name)
+	}
+}
+
+func (this *Voice) String() string {
+	if this == nil {
+		return fmt.Sprintf("<flite.Voice>{ nil }")
+	} else {
+		return fmt.Sprintf("<flite.Voice>{ name=%v }", strconv.Quote(this.Name()))
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // ERRORS
 
 func (e Error) Error() string {
 	switch e {
 	case FLITE_ERR_NONE:
 		return "FLITE_ERR_NONE"
+	case FLITE_ERR_BADPARAM:
+		return "FLITE_ERR_BADPARAM"
 	default:
 		return "[ ?? Invalid Error value ]"
 	}
